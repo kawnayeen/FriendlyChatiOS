3f5a0131afec354ad940f3e69bdd4d2c8d7a1151
kawnayeen <kawnayeen.08@gmail.com>
2018-04-24 14:55:04 +0600

    project setup with firebase sdk

---
+++ FriendlyChatiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendlyChatiOS
+//
+//  Created by Kawnayeen on 18/4/18.
+//  Copyright © 2018 Kawnayeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
